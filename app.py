00eb5a5230e1c243184a23cf6147f1b8650dcd67
Mama mia <bee@baa.s>
2023-06-07 23:37:00 +0400

    initial

---
+++ app.py
@@ -0,0 +1,31 @@
+import flask
+import json
+
+import smtplib
+from email.mime.text import MIMEText
+
+app = flask.Flask(__name__)
+send_to = 'denis@denisco.pro'
+
+
+def send_email(to, message):
+    msg = MIMEText(message)
+    msg['Subject'] = 'Registration'
+    msg['From'] = 'rega@localhost'
+    msg['To'] = to
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail('rega@localhost', [to], msg.as_string())
+    s.quit()
+
+@app.route('/')
+def index():
+    return flask.render_template('index.html')
+
+@app.route('/submit')
+def submit():
+    form = flask.request.form
+    name = form['name'] + ' ' + form['surname']
+    with open(name, 'w') as f:
+        f.write(json.dumps(form))
+    send_email(send_to, json.dumps(form))
